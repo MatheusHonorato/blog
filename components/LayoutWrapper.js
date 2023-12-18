845ea9e23d5f91cc3a60511c0801ae97c336dca2
Matheus <matheus@LAPTOP-5QQPHSHE.localdomain>
2023-12-18 12:40:21 -0300

    Update background-color webapp e favicon

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -14,10 +14,7 @@ const LayoutWrapper = ({ children }) => {
         <header className="flex items-center justify-between py-10">
           <div>
             <Link href="/" aria-label="Tailwind CSS Blog">
-              <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
-                </div>
+              <div className="flex items-center justify-between">               
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
