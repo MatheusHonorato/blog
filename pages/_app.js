a700f028d439c0aada80e596f2028f4cab76bda1
Matheus Honorato <matheuspaixaohonorato@gmail.com>
2024-09-12 10:34:47 -0300

    up

--- pages/_app.js
+++ pages/_app.js
@@ -10,10 +10,12 @@ import LayoutWrapper from '@/components/LayoutWrapper'
 import MDXComponents from '@/components/MDXComponents'
 
 export default function App({ Component, pageProps }) {
-  let fbImg =
-    'https://images.unsplash.com/photo-1599507593499-a3f7d7d97667?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  let fbImg
   if (pageProps.post !== undefined) {
     fbImg = pageProps.post.frontMatter.image
+  } else {
+    fbImg =
+      'https://images.unsplash.com/photo-1599507593499-a3f7d7d97667?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   }
   return (
     <ThemeProvider attribute="class">
